17fe5add80e8ed7eccdf4a89de909d7e09631636
Dominik Ottenbreit <dominik.ottenbreit@newmatik.com>
2024-04-12 16:45:57 +0200

    fix(create-env.sh)

--- .devcontainer/create-env.sh
+++ .devcontainer/create-env.sh
@@ -7,6 +7,10 @@ read -s APIKEY
 # Create the .env file with the API key
 echo "Creating .env file..."
 echo "APIKEY=$APIKEY" > .env
+echo "FLASK_APP=app.py" > .env
+echo "FLASK_ENV=development" > .env
+echo "FLASK_RUN_HOST=0.0.0.0" > .env
+echo "FLASK_RUN_PORT=5500" > .env
 echo ".env file created successfully."
 
 # Optional: Inform the user to restart their terminal or source the .env
