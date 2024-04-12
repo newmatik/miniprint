bf4f762f1195ae666da6c2480f978c5c7de726af
Dominik Ottenbreit <dominik.ottenbreit@newmatik.com>
2024-04-12 16:52:39 +0200

    fix(create-env)

--- .devcontainer/create-env.sh
+++ .devcontainer/create-env.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Create the .env file
-echo "Creating .env file..."
-echo "APIKEY=testapikey145" > .env
-echo "FLASK_APP=app.py" > .env
-echo "FLASK_ENV=development" > .env
-echo "FLASK_RUN_HOST=0.0.0.0" > .env
-echo "FLASK_RUN_PORT=5500" > .env
+# Define the path to the .env file. This assumes that the .devcontainer directory
+# is at the root of your project structure.
+ENV_PATH="../.env"
+
+# Create or clear the .env file
+echo "Creating or clearing .env file..."
+echo "" > $ENV_PATH
+
+# Append environment variables to the .env file
+echo "APIKEY=testapikey145" >> $ENV_PATH
+echo "FLASK_APP=app.py" >> $ENV_PATH
+echo "FLASK_ENV=development" >> $ENV_PATH
+echo "FLASK_RUN_HOST=0.0.0.0" >> $ENV_PATH
+echo "FLASK_RUN_PORT=5500" >> $ENV_PATH
 echo ".env file created successfully."
 
 # Optional: Inform the user to restart their terminal or source the .env
-echo "Please run 'source .env' to load the environment variables, or restart your terminal."
+echo "Please run 'source $ENV_PATH' to load the environment variables, or restart your terminal."
