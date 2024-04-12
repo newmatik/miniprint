100129daf463a0eb0eee2db7ee7f779185dace10
Dominik Ottenbreit <dominik.ottenbreit@newmatik.com>
2024-04-12 12:02:21 +0200

    feat(add printers)

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 # This dictionary should contain the IP and port of each printer by ID
 printers = {
     'prt-batch-TWR1': {'ip': '10.1.0.48', 'port': 9100},
+    'prt-batch-TWR2': {'ip': '10.1.0.49', 'port': 9100},
+    'prt-batch-WE1': {'ip': '10.1.0.25', 'port': 9100},
+    'prt-batch-WE2': {'ip': '10.1.0.26', 'port': 9100},
+    'prt-batch-WE3': {'ip': '10.1.0.27', 'port': 9100},
+    'prt-batch-WE4': {'ip': '10.1.0.28', 'port': 9100},
     # Add more printers as needed
 }
 
