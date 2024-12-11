06139a530837a7307c98fca0a34f08357d6347ed
jeremythegreat01 <jeremyjamessipin@gmail.com>
2024-12-11 15:09:52 +0100

    add prt-label-CDS

--- printers.py
+++ printers.py
@@ -5,6 +5,7 @@ printers = {
     'prt-batch-WE1': {'ip': '10.1.0.25', 'port': 9100},
     'prt-batch-WE2': {'ip': '10.1.0.26', 'port': 9100},
     'prt-batch-WE3': {'ip': '10.1.0.27', 'port': 9100},
-    'prt-batch-WE4': {'ip': '10.1.0.28', 'port': 9100}
+    'prt-batch-WE4': {'ip': '10.1.0.28', 'port': 9100},
+    'prt-label-CDS': {'ip': '10.1.0.53', 'port': 9100},
     # Add more printers as needed
 }
